a555e26a220e90a7aeb3d486ba55b5cb2259eeaa
Arturo <arturozamorareyes@gmail.com>
2026-09-22 06:59:35 -0600

    Calcula costo de ruta con varios puntos

--- fase3.py
+++ fase3.py
@@ -12,6 +12,16 @@ def distancia_entre_nodos(G, origen, destino):
     resultado = a_estrella(G, origen, destino, h)
     return resultado[3]
 
+def costo_ruta(G, ruta):
+    total = 0.0
+
+    for i in range(len(ruta) - 1):
+        origen = ruta[i]
+        destino = ruta[i + 1]
+        distancia = distancia_entre_nodos(G, origen, destino)
+        total = total + distancia
+
+    return total
 
 if __name__ == "__main__":
     G, G_proyectado = cargar_grafo_urbano()
@@ -24,3 +34,16 @@ if __name__ == "__main__":
     print("Origen:", origen)
     print("Destino:", destino)
     print("Distancia:", distancia, "metros")
+
+    ruta_prueba = [origen, destino]
+    costo = costo_ruta(G, ruta_prueba)
+
+    print("Costo de la ruta:", costo, "metros")
+
+    tercer_nodo = next(iter(G.successors(destino)))
+
+    ruta_tres_puntos = [origen, destino, tercer_nodo]
+    costo_tres_puntos = costo_ruta(G, ruta_tres_puntos)
+
+    print("Ruta de tres puntos:", ruta_tres_puntos)
+    print("Costo de tres puntos:", costo_tres_puntos, "metros")
